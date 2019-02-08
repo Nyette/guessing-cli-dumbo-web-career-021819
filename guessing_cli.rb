359cb419e60cc5d931138fc9f5fb285abd28d20d
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-08 22:33:23 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   random_number = rand(1..6)
   user_guess = gets.chomp
-  if user_guess
+  if user_guess.class == String
     == random_number
     puts "You guessed the correct number!"
   elsif user_guess == "exit"
