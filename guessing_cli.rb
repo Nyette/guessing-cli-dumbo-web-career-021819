323953fa2b9342fda51046c6792d971d70da910d
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-08 22:45:27 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -16,6 +16,11 @@ def run_guessing_game
     end
   elsif user_guess.class == Integer
     if user_guess == random_number
-      puts "You guesse the correct number!"
+      puts "You guessed the correct number!"
     else
-      puts 
+      puts "The computer guessed #{random_number}."
+    end
+  else
+    puts "Invalid input! Please try again!"
+  end
+end
